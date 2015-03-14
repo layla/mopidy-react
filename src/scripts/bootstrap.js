15dbe7e0f5eca697ffd6f399514efc794950b576
Koen Schmeets <k.schmeets@gmail.com>
2015-03-14 22:43:54 +0100

    Using kioc

--- src/scripts/bootstrap.js
+++ src/scripts/bootstrap.js
@@ -1,16 +1,10 @@
-var Mopidy = require('mopidy');
-var BBPromise = require('bluebird');
+import App from 'kioc';
+import config from '../../config'
 
-var app = {};
-
-app.mopidy = new BBPromise((resolve) => {
-  var mopidy = new Mopidy({
-    webSocketUrl: 'ws://musicbox.local:6680/mopidy/ws/'
-  });
-  mopidy.on(console.log.bind(console));
-  mopidy.on('state:online', () => {
-    resolve(mopidy);
-  });
+var app = new App({
+  config: config
 });
+
+app.use(require('./services/mopidy'), true);
 
 module.exports = app;
