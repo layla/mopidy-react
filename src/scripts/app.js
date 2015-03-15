3e6252ba3d5ccf5033e1d5a19abf16232b727607
Koen Schmeets <k.schmeets@gmail.com>
2015-03-15 03:12:17 +0100

    Updating routes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,11 +3,15 @@
 import React from 'react';
 import Router from 'react-router';
 let Route = Router.Route;
+let DefaultRoute = Router.DefaultRoute;
 
 let routes = (
   <Route name="root" path="/" handler={require('./ui/App')}>
     <Route name="dashboard" path="/" handler={require('./routes/Dashboard')} />
-    <Route name="search" path="search" handler={require('./routes/Search')} />
+    <Route name="search" path="search" handler={require('./routes/Search')}>
+      <Route name="search-results" path="results/:query" handler={require('./routes/SearchResults')} />
+      <DefaultRoute name="search-index" handler={require('./routes/SearchResults')} />
+    </Route>
     <Route name="playlist" path="playlist" handler={require('./routes/Playlist')} />
   </Route>
 );
