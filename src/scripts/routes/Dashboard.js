7eff53130a65157a76c9e71e5bad001b6175c0ca
Koen Schmeets <k.schmeets@gmail.com>
2015-03-16 00:48:17 +0100

    Adding player to dashboard

--- src/scripts/routes/Dashboard.js
+++ src/scripts/routes/Dashboard.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
 import {Well, Row, Col, Panel} from 'react-bootstrap';
-import {Sidebar} from '../ui';
+import {Sidebar, Player} from '../ui';
+
+import app from '../bootstrap';
 
 let Dashboard = React.createClass({
   render() {
     return (
       <Row>
         <Col lg={3} md={4} sm={4} xs={12}>
-          <Panel bsStyle="primary">
+          <Panel bsStyle="primary" header="Menu">
             <Sidebar />
           </Panel>
         </Col>
         <Col lg={9} md={8} sm={8} xs={12}>
-          <Well>Dash</Well>
+          <Well>
+            <Player />
+          </Well>
         </Col>
       </Row>
     );
