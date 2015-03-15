61660e10d4fae88d9b4449ebd50977ae04fae573
Koen Schmeets <k.schmeets@gmail.com>
2015-03-16 00:49:40 +0100

    Updating filterable search

--- src/scripts/ui/FilterableTracks.js
+++ src/scripts/ui/FilterableTracks.js
@@ -15,7 +15,8 @@ export default React.createClass({
     return {
       tracks: this.props.tracks,
       provider: 'all',
-      search: null
+      search: null,
+      searchPrefix: ''
     }
   },
 
@@ -30,46 +31,47 @@ export default React.createClass({
   shouldComponentUpdate(nextProps, nextState) {
     var shouldUpdate = this.props.search !== nextProps.search ||
       this.state.tracks !== nextProps.tracks ||
-      this.state.provider !== nextState.provider;
+      this.state.provider !== nextState.provider ||
+      this.state.searchPrefix !== nextProps.searchPrefix;
     console.log('FilterableTracks.shouldComponentUpdate', shouldUpdate);
     return shouldUpdate;
   },
 
   filterTracks(tracks) {
-    let filters = [
-      (track) => {
-        return this.state.provider === 'all' ||
-          this.state.provider === track.uri.split(':')[0];
-      },
-      (track) => {
-        if (this.state.search === null) {
-          return true;
-        }
+    console.log('FilterableTracks.filterTracks');
+    return _.filter(tracks, (track) => {
+      let providerCheck = this.state.provider === 'all' ||
+        this.state.provider === track.uri.split(':')[0];
 
-        var searchString = [
+      let searchCheck;
+      if (this.state.search === null) {
+        searchCheck = true;
+      } else {
+        var haystack = [
           'track:' + (track.name ? track.name : '-'),
           'artist:' + (track.artists ? _.first(track.artists).name : '-'),
           'album:' + (track.album ? track.album.name : '-'),
           'date:' + (track.album ? track.album.date : '-')
         ].join(' ');
         
-        return fuzzy.test(this.state.search, searchString);
+        var searchString = this.state.searchPrefix + this.state.search;
+        console.log('searching', searchString);
+        searchCheck = fuzzy.test(searchString, haystack);
       }
-    ];
-
-    return _.filter(tracks, (track) => {
-      let filterResults = _.map(filters, (filter) => {
-        return filter(track);
-      });
-      console.log('filterResults', filterResults);
-      let passes = ! _.contains(filterResults, false);
-      return passes;
+      
+      return providerCheck && searchCheck;
     });
   },
 
-  filterByProvider(provider) {
+  filterProvider(provider) {
     this.setState({
       provider: provider
+    });
+  },
+
+  filterSearch(searchPrefix) {
+    this.setState({
+      searchPrefix: searchPrefix
     });
   },
 
@@ -78,23 +80,29 @@ export default React.createClass({
     console.log('FilterableTracks.render', filteredTracks.length);
     return (
       <div>
-        <span>FILTER</span>
+        <b>FILTER PROVIDER</b>
         <ul className="nav nav-pills">
-          <li className={this.state.provider === 'all' ? 'active' : ''}><a onClick={this.filterByProvider.bind(this, 'all')}>all</a></li>
-          <li className={this.state.provider === 'spotify' ? 'active' : ''}><a onClick={this.filterByProvider.bind(this, 'spotify')}>spotify</a></li>
-          <li className={this.state.provider === 'youtube' ? 'active' : ''}><a onClick={this.filterByProvider.bind(this, 'youtube')}>youtube</a></li>
-          <li className={this.state.provider === 'soundcloud' ? 'active' : ''}><a onClick={this.filterByProvider.bind(this, 'soundcloud')}>soundcloud</a></li>
-          <li className={this.state.provider === 'gmusic' ? 'active' : ''}><a onClick={this.filterByProvider.bind(this, 'gmusic')}>gmusic</a></li>
+          <li className={this.state.provider === 'all' ? 'active' : ''}><a onClick={this.filterProvider.bind(this, 'all')}>all</a></li>
+          <li className={this.state.provider === 'spotify' ? 'active' : ''}><a onClick={this.filterProvider.bind(this, 'spotify')}>spotify</a></li>
+          <li className={this.state.provider === 'youtube' ? 'active' : ''}><a onClick={this.filterProvider.bind(this, 'youtube')}>youtube</a></li>
+          <li className={this.state.provider === 'soundcloud' ? 'active' : ''}><a onClick={this.filterProvider.bind(this, 'soundcloud')}>soundcloud</a></li>
+          <li className={this.state.provider === 'gmusic' ? 'active' : ''}><a onClick={this.filterProvider.bind(this, 'gmusic')}>gmusic</a></li>
+        </ul>
+        <br />
+        <b>FILTER FUZZY SEARCH</b>
+        <ul className="nav nav-pills">
+          <li className={this.state.searchPrefix === '' ? 'active' : ''}><a onClick={this.filterSearch.bind(this, '')}>all</a></li>
+          <li className={this.state.searchPrefix === 'track:' ? 'active' : ''}><a onClick={this.filterSearch.bind(this, 'track:')}>track</a></li>
+          <li className={this.state.searchPrefix === 'artist:' ? 'active' : ''}><a onClick={this.filterSearch.bind(this, 'artist:')}>artist</a></li>
+          <li className={this.state.searchPrefix === 'album:' ? 'active' : ''}><a onClick={this.filterSearch.bind(this, 'album:')}>album</a></li>
+          <li className={this.state.searchPrefix === 'date:' ? 'active' : ''}><a onClick={this.filterSearch.bind(this, 'date:')}>date</a></li>
         </ul>
         <br />
         { filteredTracks.length > 0 ? (
           <Tracks tracks={filteredTracks} />
         ) : (
           <center>
-            <b>Your search did not return any results.</b>
-            { _.isArray(this.state.tracks) && this.state.tracks.length > 0 ? (
-              <div>Hit the search button to start a new search with the query "{ this.state.search }"</div>
-            ) : '' }
+            <b>Your search / filter combination did not return any results.</b>
           </center>
         ) }
       </div>
